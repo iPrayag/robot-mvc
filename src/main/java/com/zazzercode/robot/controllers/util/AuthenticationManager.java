7a7939f8c421bc0be81fe596614debcdb0857fa5
iPrayag <prayag.upd@gmail.com>
2013-06-30 00:13:53 +0545

    ADD AuthManager and RobotException

---
+++ src/main/java/com/zazzercode/robot/controllers/util/AuthenticationManager.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.zazzercode.robot.controllers.util;
+
+import java.security.Key;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
+/**
+ * @author prayag
+ * 
+ */
+public class AuthenticationManager {
+	private static String algorithm = "DESede";
+	private static Key key = null;
+	private static Cipher cipher = null;
+
+	private static byte[] encrypt(String input) throws Exception {
+		key = KeyGenerator.getInstance(algorithm).generateKey();
+		cipher = Cipher.getInstance(algorithm);
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] inputBytes = input.getBytes();
+		return cipher.doFinal(inputBytes);
+	}
+
+	private static String decrypt(byte[] encryptionBytes) throws Exception {
+		key = KeyGenerator.getInstance(algorithm).generateKey();
+		cipher = Cipher.getInstance(algorithm);
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] recoveredBytes = cipher.doFinal(encryptionBytes);
+		String recovered = new String(recoveredBytes);
+		return recovered;
+	}
+}
---
+++ src/main/java/com/zazzercode/robot/controllers/util/RobotException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.zazzercode.robot.controllers.util;
+
+/**
+ * @author prayag
+ * 
+ */
+public class RobotException extends Exception {
+	private String message;
+
+	public RobotException(String message) {
+		super(message);
+	}
+
+}
