3951edbb5f6b13a6dd10e4b511b7967b12fcfca2
parayaluyanta <prayag.upd@gmail.com>
2021-06-27 11:07:22 -0700

    update to use JDK11

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+Robot MVC
+-------------
+- a simple swing app to demonstrate MVC framework
 
-<b>Download Dependencies</b>
+<b>Download Java Dependencies</b>
 
 `$ mvn install`
 
--- src/main/java/com/zazzercode/robot/models/util/RobotScheduler.java
+++ src/main/java/com/zazzercode/robot/models/util/RobotScheduler.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.zazzercode.robot.models.util;
 
@@ -10,20 +10,20 @@ import org.quartz.impl.StdSchedulerFactory;
 
 /**
  * @author prayag
- * 
+ *
  */
 public class RobotScheduler {
-	public static void startSchedule() throws Exception {
-		JobDetail jobDetail = new JobDetail("robotJob", null, RobotJob.class);
+    public static void startSchedule() throws Exception {
+        JobDetail jobDetail = new JobDetail("robotJob", null, RobotJob.class);
 
-		CronTrigger trigger = new CronTrigger();
-		trigger.setName("robotTrigger");
-		trigger.setCronExpression("0/50 * * * * ?");
+        CronTrigger trigger = new CronTrigger();
+        trigger.setName("robotTrigger");
+        trigger.setCronExpression("0/50 * * * * ?");
 
-		// schedule it
-		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-		scheduler.start();
-		scheduler.scheduleJob(jobDetail, trigger);
+        // schedule it
+        Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+        scheduler.start();
+        scheduler.scheduleJob(jobDetail, trigger);
 
-	}
+    }
 }
